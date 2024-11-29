143c10525b4e563bc85bf95c746b904a22e316fc
simasguga <simasguga@gmail.com>
2024-11-29 15:06:26 -0300

    ajuste diretorio array

--- array/array.go
+++ array/array.go
@@ -1,32 +1,36 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// declaração de array do tipo any, aceitando qualquer tipo de dado
-	var arr []any
-	arr = append(arr, 1, "dois", 3, 4, "cinco", 6, 7, 8, 9, 10)
 
-	// acessando o valor de um indice a outro.
-	// fmt.Println(arr[3:6])
+	// Declaração de array do tipo any, aceitando qualquer tipo de dado
+	var tipoQualquer []any
+	var tipoString []string
+	var tipoNumerico []int
 
-	// loop para exibir todos os indices.
-	// i := 0
-	// for i < len(arr) {
-	// 	fmt.Println(arr[i])
-	// 	i++
-	// }
+	// Atribuir valores ao array de qualquer tipo
+	tipoQualquer = append(tipoQualquer, 1, "dois", 3, 4, "cinco", 6, 7, 8, 9, 10)
 
-	// adicionando novos indices ao array existente
+	// Atribuir valores ao array de string
+	tipoString = append(tipoString, "Jorge", "Maria", "Felicio", "Ana", "Michele")
+
+	// Atribuir valores ao array de números
+	tipoNumerico = append(tipoNumerico, 2, 3, 5, 8, 12, 11, 9)
+
+	// Adicionando novos índices ao array existente
 	var arr2 = []any{11, "doze", 13, 14, 15}
-	arr = append(arr, arr2)
-	i := 0
-	// for i < len(arr) {
-	// 	fmt.Println(arr[i])
-	// 	i++
-	// }
+	tipoQualquer = append(tipoQualquer, arr2)
 
-	// acessando array dentro de outro array
-	for i < len(arr) {
-
+	// Loop para acessar e verificar o tipo dos elementos
+	for i := 0; i < len(tipoQualquer); i++ {
+		switch v := tipoQualquer[i].(type) {
+		case []any: // Verifica se o tipo é um slice de "any"
+			fmt.Printf("Elemento no índice %d é um array: %v\n", i, v)
+		default:
+			// Ignorar outros tipos
+		}
 	}
-
 }
