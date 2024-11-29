b150adbbba72bac928da1e3fa8ec3597f45baef3
simasguga <simasguga@gmail.com>
2024-11-29 09:47:02 -0300

    diretorio estruturafor e respectivo arquivo

---
+++ estruturafor/estruturafor.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Estrutura simples do for para printar de 0 a 10
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+// Comando para executar no terminal -- go run estruturafor.go
